7699a36249af414e368c14ec4451a740dfdd9d86
DevSORA <sodaoov@gmail.com>
2021-08-12 18:51:20 +0900

    env: creat_react_element,page shell script 생성 feat: DiaryCard 컴포넌트 생성 feat: MainPage 컴포넌트 생성 feat: SignIn/UpPage 폴더 이름 변경
    :wq

---
+++ client/scripts/create_react_page.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+if [ -z "$*" ]; then
+ echo "put argument with PageName"
+ exit 0
+
+fi
+
+FOLDER="Pages"
+FILE_NAME=$1
+FIRST_CAP="$(tr '[:lower:]' '[:upper:]' <<< ${FOLDER:0:1})${FOLDER:1}"
+APPEND=`echo "${FIRST_CAP%?}"`
+
+echo `mkdir src/${FOLDER}/${FILE_NAME}`
+
+# react file
+echo `echo "import React from 'react';
+import styled from 'styled-components';
+
+interface ${FILE_NAME}Props {
+
+}
+
+const ${FILE_NAME} = ({} : ${FILE_NAME}Props) => {
+	return (
+    <${FILE_NAME}Container>
+      ${FILE_NAME}
+    </${FILE_NAME}Container>
+  )
+};
+
+export default ${FILE_NAME};
+
+const ${FILE_NAME}Container = styled.div\\\`\\\`;
+" > src/${FOLDER}/${FILE_NAME}/${FILE_NAME}.tsx`
---
+++ client/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface MainPageProps {
+
+}
+
+const MainPage = ({} : MainPageProps) => {
+	return (
+    <MainPageContainer>
+      MainPage
+    </MainPageContainer>
+  )
+};
+
+export default MainPage;
+
+const MainPageContainer = styled.div``;
+

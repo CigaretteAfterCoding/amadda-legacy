7c3505e139ced3b711256a8edafb06f35106afce
Bonggeun Jeong <jungcome7@gmail.com>
2021-09-08 17:33:05 +0900

    feat: 클라이언트 빌드 스크립트 추가

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,19 +7,27 @@ import cookieParser from 'cookie-parser';
 import router from 'Routes/';
 import { errorHandler } from 'Middlewares/error-handler';
 import cors from 'cors';
+import path from 'path';
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(morgan('dev'));
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'http://localhost:4000', credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
 
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+
 app.use('/api', router);
 app.use(errorHandler);
+
+app.use((req, res) => {
+  console.log(path.join(__dirname, '../../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));

62157b36138734ab592b8a041701e517b26c59ae
DevSORA <sodaoov@gmail.com>
2021-09-13 00:23:30 +0900

    feat:로그인, 회원가입 배경 동영상 날씨에 맞게 나타내기

---
+++ client/src/apis/weatherAPI.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const BASE_URI =
+  'https://api.openweathermap.org/data/2.5/weather?q=SEOUL&appid=e7b1a57cd2158c8d195bfb24b7597bad&lang=kr&units=metric';
+
+const getWeather = async () => {
+  try {
+    const { data } = await axios.get(BASE_URI);
+    if (data.weather[0].main === 'Clear') return 0;
+    if (data.weather[0].main === 'Clouds') return 1;
+    if (data.weather[0].main === 'Rain') return 2;
+    if (data.weather[0].main === 'Snow') return 3;
+    return 0;
+  } catch (err) {
+    console.log(err);
+    return 0;
+  }
+};
+
+export default { getWeather };

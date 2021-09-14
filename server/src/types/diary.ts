c11cb75ef7d1c78fdf40bfa6084c27f9f9387eeb
Bonggeun Jeong <jungcome7@gmail.com>
2021-09-14 18:40:27 +0900

    fix: is_private을 불리언으로 응답하도록 수정

--- server/src/types/diary.ts
+++ server/src/types/diary.ts
@@ -1,9 +1,19 @@
-export interface Diary {
+export interface DiaryParams {
   id?: number;
   title: string;
   content: string;
   date: string;
   weather: string;
   isPrivate: 0 | 1;
+  userId: number;
+}
+
+export interface DiaryResponse {
+  id?: number;
+  title: string;
+  content: string;
+  date: string;
+  weather: string;
+  is_private: 0 | 1 | boolean;
   userId: number;
 }
